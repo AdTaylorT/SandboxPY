efe33434d530ceac9dccaf62a68246da72156f6f
s7ae1k3r <34325950+s7ae1k3r@users.noreply.github.com>
2019-04-30 22:21:14 -0400

    Update solution.py
    only need one method,

--- solution.py
+++ solution.py
@@ -1,17 +1,15 @@
 # Forcing recursion for no good reason.  But it passed so....
-
-def solution_r(n):
+def solution(number):
+    if not number:
+        return 0
+    
     if n <= 0:
         return n
     else:
         if not n%3 or not n%5:
-            return n + solution_r(n-1)
+            return n + solution_r(number-1)
         else:
-            return solution_r(n-1)
+            return solution(number-1)
+    return solution(number-1)
 
-def solution(number):
-    if not number:
-        return 0
-    return solution_r(number-1)
-
-assert solution(10) == 23, "Oops, recursion is the devil"
+assert(solution(10) == 23, "Yay")
